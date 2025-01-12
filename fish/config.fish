2eb92e73d2fff9330bb76727e5dbe65180d5ca86
Jaycorn <jaycorn@pop-os.fritz.box>
2025-01-12 14:00:55 +0100

    Added upsync scipt to sync dotfiles to repo

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,12 @@ end
 
 set -U fish_greeting 'Moinsen ;)'
 
+### ALIASES
+alias pw='dcli password'
+alias jvnc='x11vnc -display :1 -auth guess -forever -loop -noxdamage -repeat - passwd pwd -rfbport 5901'
+alias obsidian='~/Obsidian'
+alias fs='starship init fish | source'
 
+# init starship prompt on startup
+#
+# starship init fish | source
---
+++ upsync.bash
@@ -0,0 +1,6 @@
+cp -rf ~/.config/starship.toml ~/.config/starship/active_starship.toml
+
+cp -rf ~/.config/nvim ~/Documents/dotfiles
+cp -rf ~/.config/fish ~/Documents/dotfiles
+cp -rf ~/.config/tmux ~/Documents/dotfiles
+cp -rf ~/.config/starship ~/Documents/dotfiles
