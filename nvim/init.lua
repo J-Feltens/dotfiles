3c9d2d27af379288933e214a2fc032491c156367
Jaycorn <jaycorn@pop-os.fritz.box>
2025-01-13 10:55:20 +0100

    Added plugin cursor highlight for nvim, added keybinds for nvim to navigate windows using space+arrows

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,10 +17,11 @@ vim.o.ttyfast = true
 require('config.lazy')
 
 
+
 -- highlight text when copying ('y' or 'yy')
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight text when yanking',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {clear = true}),
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
@@ -30,8 +31,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- color theme
 -- vim.cmd[colorscheme tokyonight-moon]]
-vim.cmd[[colorscheme kanagawa]]
-vim.cmd[[NvimTreeOpen]]
+vim.cmd [[colorscheme kanagawa]]
+vim.cmd [[NvimTreeOpen]]
 
 -- custom keybinds
 local builtin = require('telescope.builtin')
@@ -39,10 +40,18 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', { desc = "Toggle NvimTree" })
 
+local opts = { noremap = true, silent = true }
+
+-- Window navigation
+vim.api.nvim_set_keymap('n', '<Leader><Up>', '<C-w>k', opts)    -- Navigate up
+vim.api.nvim_set_keymap('n', '<Leader><Down>', '<C-w>j', opts)  -- Navigate down
+vim.api.nvim_set_keymap('n', '<Leader><Left>', '<C-w>h', opts)  -- Navigate left
+vim.api.nvim_set_keymap('n', '<Leader><Right>', '<C-w>l', opts) -- Navigate right
+
 vim.keymap.set('n', '<leader>fc', function()
-	builtin.find_files({
-		cwd = vim.fn.stdpath('config'),
-	})
+		builtin.find_files({
+			cwd = vim.fn.stdpath('config'),
+		})
 	end,
 	{
 		desc = 'Search in nvim config files'
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -174,4 +174,21 @@ return {
 			})
 		end,
 	},
+	{
+		"ya2s/nvim-cursorline",
+		config = function()
+			require("nvim-cursorline").setup({
+				cursorline = {
+					enable = true,
+					timeout = 0,
+					number = true, -- Set to true if you want the line number to highlight with the cursor line
+				},
+				cursorword = {
+					enable = true,
+					min_length = 2, -- Minimum word length to highlight
+					hl = { underline = true },
+				},
+			})
+		end,
+	},
 }
