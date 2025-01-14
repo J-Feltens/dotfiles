5dc86433c8eac8e91641a76396f5f37f2314aee0
Jaycorn <jaycorn@pop-os.fritz.box>
2025-01-15 00:28:18 +0100

    Added path variable to upsync

--- upsync.sh
+++ upsync.sh
@@ -1,13 +1,17 @@
+$DIR="~/dotfiles"
+
 echo "Upsyncing system files to git repo may overwrite repository."
 read -p "Continue? (y/n) " -n 1 -r
-echo    # (optional) move to a new line
+echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	cp -rf ~/.config/nvim ~/Documents/dotfiles
-	cp -rf ~/.config/fish ~/Documents/dotfiles
-	cp -rf ~/.config/tmux ~/Documents/dotfiles
+	cp -rf ~/.config/nvim ~/$DIR/dotfiles
+	cp -rf ~/.config/fish ~/$DIR/dotfiles
+	cp -rf ~/.config/tmux ~/$DIR/dotfiles
+
+	# copy current starship theme into theme directory	
 	cp -rf ~/.config/starship.toml ~/.config/starship/active_starship.toml
-	cp -rf ~/.config/starship ~/Documents/dotfiles
+	cp -rf ~/.config/starship ~/$DIR/dotfiles
 	echo 'Done :)'
 else
 	echo Aborting...
