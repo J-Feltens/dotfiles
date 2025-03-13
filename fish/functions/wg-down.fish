0c01d8af9ece3820be47ffaaa462ca73a2a42ad6
James Feltens <james@pop-os.net.golden-devices.de>
2025-03-13 16:23:34 +0100

    Added fish functgions for wireguard quick connect and disconnect

---
+++ fish/functions/wg-down.fish
@@ -0,0 +1,3 @@
+function wg-down --description "alias for wg-quick down wg0"
+	sudo wg-quick down wg0
+end
---
+++ fish/functions/wg-up.fish
@@ -0,0 +1,3 @@
+function wg-up --description "alias for wg-quick up wg0"
+	sudo wg-quick up wg0
+end
