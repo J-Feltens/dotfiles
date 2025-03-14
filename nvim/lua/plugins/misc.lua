c26260a6654dc943c8d3a0630ba02af387eb952b
James Feltens <james@pop-os.net.golden-devices.de>
2025-03-14 15:16:51 +0100

    Added nvim plugin for ctrl d vscode like behaviour

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -25,4 +25,30 @@ return {
 			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
 		},
 	},
+	{
+		"jake-stewart/multicursor.nvim",
+		branch = "1.0",
+		config = function()
+			local mc = require("multicursor-nvim")
+			mc.setup()
+
+			local set = vim.keymap.set
+
+			-- Add or skip adding a new cursor by matching word/selection
+			set({ "n", "x" }, "<leader>n", function() mc.matchAddCursor(1) end)
+
+			-- Disable and enable cursors.
+			set({ "n", "x" }, "<leader>N", mc.toggleCursor)
+
+			-- Customize how cursors look.
+			local hl = vim.api.nvim_set_hl
+			hl(0, "MultiCursorCursor", { link = "Cursor" })
+			hl(0, "MultiCursorVisual", { link = "Visual" })
+			hl(0, "MultiCursorSign", { link = "SignColumn" })
+			hl(0, "MultiCursorMatchPreview", { link = "Search" })
+			hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
+			hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+			hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+		end
+	}
 }
